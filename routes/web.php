4ecd468e661d483cc385ccc81b6b7fe66fede46f
Ardana Yatra <imadeardanayatra@gmail.com>
2025-01-20 10:49:19 +0800

    fix route

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,43 @@ use App\Http\Controllers\Api\MediaController;
 use App\Http\Controllers\Api\TagController;
 
 Route::prefix('api')->group(function () {
-Route::apiResource('blogs', BlogController::class);
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('tags', TagController::class);
-Route::apiResource('comments', CommentController::class);
-Route::apiResource('likes', LikeController::class);
-Route::apiResource('media', MediaController::class);
+    // Blog routes
+    Route::get('blogs', [BlogController::class, 'index']);
+    Route::get('blogs/{id}', [BlogController::class, 'show']);
+    Route::post('blogs', [BlogController::class, 'store']);
+    Route::put('blogs/{id}', [BlogController::class, 'update']);
+    Route::delete('blogs/{id}', [BlogController::class, 'destroy']);
+
+    // Other resource routes
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('categories/{id}', [CategoryController::class, 'show']);
+    Route::post('categories', [CategoryController::class, 'store']);
+    Route::put('categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+
+    Route::get('tags', [TagController::class, 'index']);
+    Route::get('tags/{id}', [TagController::class, 'show']);
+    Route::post('tags', [TagController::class, 'store']);
+    Route::put('tags/{id}', [TagController::class, 'update']);
+    Route::delete('tags/{id}', [TagController::class, 'destroy']);
+
+    Route::get('comments', [CommentController::class, 'index']);
+    Route::get('comments/{id}', [CommentController::class, 'show']);
+    Route::post('comments', [CommentController::class, 'store']);
+    Route::put('comments/{id}', [CommentController::class, 'update']);
+    Route::delete('comments/{id}', [CommentController::class, 'destroy']);
+
+    Route::get('likes', [LikeController::class, 'index']);
+    Route::get('likes/{id}', [LikeController::class, 'show']);
+    Route::post('likes', [LikeController::class, 'store']);
+    Route::put('likes/{id}', [LikeController::class, 'update']);
+    Route::delete('likes/{id}', [LikeController::class, 'destroy']);
+
+    Route::get('media', [MediaController::class, 'index']);
+    Route::get('media/{id}', [MediaController::class, 'show']);
+    Route::post('media', [MediaController::class, 'store']);
+    Route::put('media/{id}', [MediaController::class, 'update']);
+    Route::delete('media/{id}', [MediaController::class, 'destroy']);
 });
 // Rute untuk homepage
 Route::get('/', function () {
